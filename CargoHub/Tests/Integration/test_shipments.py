999d57f39ed4a70111f796efa71522c3ca497f54
Max Bretherton <maxob2005@gmail.com>
2024-10-10 15:53:00 +0200

    Finish shipment integration tests #4
    There still are tests, where I dont know what the code does, so I am skipping it for now

--- CargoHub/Tests/Integration/test_shipments.py
+++ CargoHub/Tests/Integration/test_shipments.py
@@ -67,13 +67,13 @@ def checkShipmentItem(shipment_item):
 class TestClass(unittest.TestCase):
     def setUp(self):
         self.client = httpx
-        self.url = "http://localhost:3000/api/v1"
+        self.base_url = "http://localhost:3000/api/v1"
         self.headers = httpx.Headers({ 'API_KEY': 'a1b2c3d4e5' })
 
 
     def test_get_shipments(self): 
         # Stuur de request
-        response = self.client.get(url=(self.url + "/shipments"), headers=self.headers)
+        response = self.client.get(url=(self.base_url + "/shipments"), headers=self.headers)
         
         # Check de status code
         self.assertEqual(response.status_code, 200)
@@ -93,7 +93,7 @@ class TestClass(unittest.TestCase):
 
     def test_get_shipment(self):
         # Stuur de request
-        response = self.client.get(url=(self.url + "/shipments/1"), headers=self.headers)
+        response = self.client.get(url=(self.base_url + "/shipments/1"), headers=self.headers)
         
         # Check de status code
         self.assertEqual(response.status_code, 200)
@@ -108,7 +108,7 @@ class TestClass(unittest.TestCase):
     
     def test_get_shipment_orders(self):
         # Stuur de request
-        response = self.client.get(url=(self.url + "/shipments/1/orders"), headers=self.headers)
+        response = self.client.get(url=(self.base_url + "/shipments/1/orders"), headers=self.headers)
         
         # Check de status code
         self.assertEqual(response.status_code, 200)
@@ -121,13 +121,15 @@ class TestClass(unittest.TestCase):
             # Check of de list gevuld is met ints
             self.assertEqual(type(response.json()[0]), int)
             
-            # Check of de order shipment_id 1 is
-            pass
+            # Check of de shipment_id van de order 1
+            order_id = response.json()[0]
+            response = self.client.get(url=(self.base_url + f"/orders/{order_id}"), headers=self.headers)
+            self.assertEqual(response.json()["shipment_id"], 1)
             
 
     def test_get_shipment_items(self):
         # Stuur de request
-        response = self.client.get(url=(self.url + "/shipments/1/items"), headers=self.headers)
+        response = self.client.get(url=(self.base_url + "/shipments/1/items"), headers=self.headers)
         
         # Check de status code
         self.assertEqual(response.status_code, 200)
@@ -145,15 +147,15 @@ class TestClass(unittest.TestCase):
             self.assertTrue(checkShipmentItem(response.json()[0]))
     
         # Check of de items ook de items in de shipment zijn
-        #! Zou eigenlijk moeten doen via het lezen van de database
+        # Zou eigenlijk moeten doen via het lezen van de database
         self.assertEqual(response.json(),
-            self.client.get(url=(self.url + "/shipments/1"), headers=self.headers).json()["items"])
+            self.client.get(url=(self.base_url + "/shipments/1"), headers=self.headers).json()["items"])
         
     
     def test_post_shipment(self):
         # Shipment object
         data = {
-            "id": 99999,
+            "id": 6,
             "order_id": None,
             "source_id": None,
             "order_date": None,
@@ -175,20 +177,22 @@ class TestClass(unittest.TestCase):
         }
         
         # Stuur de request
-        response = self.client.post(url=(self.url + "/shipments"), headers=self.headers, json=data)
+        response = self.client.post(url=(self.base_url + "/shipments"), headers=self.headers, json=data)
         
         # Check de status code
         self.assertEqual(response.status_code, 201)
         
         # Check of de shipment in de database zit
-        pass
+        response = self.client.get(url=(self.base_url + "/shipments/6"), headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["id"], 6)
         
 
     def test_put_shipment(self):
         #! We gebruiken nog geen test-database, dus dit kan niet echt getest worden
         
         data = {
-            "id": 99999,
+            "id": 2,
             "order_id": None,
             "source_id": None,
             "order_date": None,
@@ -210,29 +214,42 @@ class TestClass(unittest.TestCase):
         }
         
         # Stuur de request
-        response = self.client.put(url=(self.url + "/shipments/1"), headers=self.headers, json=data)
+        response = self.client.put(url=(self.base_url + "/shipments/1"), headers=self.headers, json=data)
         
         # Check de status code
         self.assertEqual(response.status_code, 200)
         
         # Check of de shipment in de database is aangepast
-        pass
+        response = self.client.get(url=(self.base_url + "/shipments/2"), headers=self.headers)
+        self.assertEqual(response.json()["notes"], "This shipment has been modified")
 
 
     def test_put_shipment_orders(self):
         #! We gebruiken nog geen test-database, dus dit kan niet echt getest worden
         # Array van order id's
-        data = [1, 2, 3]
+        data = [4]
         
         # Stuur de request
-        response = self.client.put(url=(self.url + "/shipments/1/orders"), headers=self.headers, json=data)
+        response = self.client.put(url=(self.base_url + "/shipments/3/orders"), headers=self.headers, json=data)
         
         # Check de status code
         self.assertEqual(response.status_code, 200)
         
-        # Check of de bestaande orders in de shipment, die niet meegegeven werden in de data op
-        # shipment_id = -1 en shipment_status = "Scheduled" staat
-        pass
+        # Check of de shipment aangepast is
+        response = self.client.get(url=(self.base_url + "/shipments/3/orders"), headers=self.headers)
+        self.assertEqual(response.json(), data)
+        
+        # Check of de bestaande order in de shipment, die niet meegegeven is in de data op
+        # shipment_id = -1 en order_status = "Scheduled" staat
+        response = self.client.get(url=(self.base_url + f"/orders/3"), headers=self.headers)
+        self.assertEqual(response.json()["shipment_id"], -1)
+        self.assertEqual(response.json()["order_status"], "Scheduled")
+        
+        # Check of de bestaande order, die wel meegegeven is in de data op
+        # shipment_id = 3 en order_status = "Packed" staat
+        response = self.client.get(url=(self.base_url + f"/orders/4"), headers=self.headers)
+        self.assertEqual(response.json()["shipment_id"], 3)
+        self.assertEqual(response.json()["order_status"], "Packed")
     
     
     
@@ -240,7 +257,7 @@ class TestClass(unittest.TestCase):
         # Ik weet niet wat dit doet
         
         # Stuur de request
-        #response = self.client.put(url=(self.url + "/shipments/1/items"), headers=self.headers, json=data)
+        #response = self.client.put(url=(self.base_url + "/shipments/1/items"), headers=self.headers, json=data)
         
         # Check de status code
         #self.assertEqual(response.status_code, 200)
@@ -252,7 +269,7 @@ class TestClass(unittest.TestCase):
         # Doet niks in de code
         
         # Stuur de request
-        #response = self.client.put(url=(self.url + "/shipments/1/commit"), headers=self.headers, json=data)
+        #response = self.client.put(url=(self.base_url + "/shipments/1/commit"), headers=self.headers, json=data)
         
         # Check de status code
         #self.assertEqual(response.status_code, 200)
@@ -262,10 +279,11 @@ class TestClass(unittest.TestCase):
 
     def test_delete_shipment(self):
          # Stuur de request
-        response = self.client.delete(url=(self.url + "/shipments/1"), headers=self.headers)
+        response = self.client.delete(url=(self.base_url + "/shipments/5"), headers=self.headers)
         
         # Check de status code
         self.assertEqual(response.status_code, 200)
         
         # Check of de shipment uit de database is
-        pass
+        response = self.client.get(url=(self.base_url + "/shipments/5"), headers=self.headers)
+        self.assertEqual(response.json(), None)
