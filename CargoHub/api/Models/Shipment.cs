45fb2783d6ebc71dcde8e88e79403646fc6a8abd
Max Bretherton <76232522+MaxBretherton@users.noreply.github.com>
2024-10-15 10:40:17 +0200

    Create base for all models #6

---
+++ CargoHub/api/Models/Shipment.cs
@@ -0,0 +1,57 @@
+using System.Text.Json.Serialization;
+
+public class Shipment : Base
+{
+
+    [JsonPropertyName("id")]
+    public required int Id { get; set; }
+    
+    [JsonPropertyName("order_id")]
+    public required int OrderId { get; set; }
+
+    [JsonPropertyName("source_id")]
+    public required int SourceId { get; set; }
+
+    [JsonPropertyName("order_date")]
+    public required string OrderDate { get; set; }
+
+    [JsonPropertyName("request_date")]
+    public required string RequestDate { get; set; }
+
+    [JsonPropertyName("shipment_date")]
+    public required string ShipmentDate { get; set; }
+
+    [JsonPropertyName("shipment_type")]
+    public required string ShipmentType { get; set; }
+
+    [JsonPropertyName("shipment_status")]
+    public required string ShipmentStatus { get; set; }
+
+    [JsonPropertyName("notes")]
+    public required string Notes { get; set; }
+
+    [JsonPropertyName("carrier_code")]
+    public required string CarrierCode { get; set; }
+
+    [JsonPropertyName("carrier_description")]
+    public required string CarrierDescription { get; set; }
+
+    [JsonPropertyName("service_code")]
+    public required string ServiceCode { get; set; }
+
+    [JsonPropertyName("payment_type")]
+    public required string PaymentType { get; set; }
+    
+    [JsonPropertyName("transfer_mode")]
+    public required string TransferMode { get; set; }
+
+    [JsonPropertyName("total_package_count")]
+    public required int TotalPackageCount { get; set; }
+
+    [JsonPropertyName("total_package_weight")]
+    public required int TotalPackageWeight { get; set; }
+
+    [JsonPropertyName("items")]
+    public required List<ItemSmall> Items { get; set; }
+
+}
