9144329a3fdbd0ad3557b9819c1ff257ea077b22
Max Bretherton <maxob2005@gmail.com>
2024-10-17 15:40:03 +0200

    Add example method for new Service/Provider way #11

--- CargoHub/api/DataProviders/ClientProvider.cs
+++ CargoHub/api/DataProviders/ClientProvider.cs
@@ -1,4 +1,12 @@
 public class ClientProvider : BaseProvider<Client>, IClientProvider
 {
     public ClientProvider() : base("data/clients.json") {}
+
+    public Client[] Get(){
+        return context.ToArray();
+    }
+
+    public void Add(Client client){
+        context.Add(client);
+    }
 }
