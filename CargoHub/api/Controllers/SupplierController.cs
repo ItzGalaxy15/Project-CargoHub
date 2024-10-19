8d898d6704ac96c23ff3bc7416a095abe6f113be
Max Bretherton <maxob2005@gmail.com>
2024-10-19 14:29:57 +0200

    Create get suppliers endpoint

--- CargoHub/api/Controllers/SupplierController.cs
+++ CargoHub/api/Controllers/SupplierController.cs
@@ -3,5 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/v1/suppliers")]
 public class SupplierController : Controller
 {
+    private readonly ISupplierService _supplierService;
+    public SupplierController(ISupplierService supplierService){
+        _supplierService = supplierService;
+    }
 
+    [HttpGet]
+    public async Task<IActionResult> GetSuppliers(){
+        return Ok(_supplierService.GetSuppliers());
+    }
 }
