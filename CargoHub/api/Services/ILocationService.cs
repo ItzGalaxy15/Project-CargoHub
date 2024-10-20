9140059104d2b304278064c724d220c08139631a
Jordy <jordym_1999@hotmail.com>
2024-10-20 13:04:40 +0200

    Added post for location #93

--- CargoHub/api/Services/ILocationService.cs
+++ CargoHub/api/Services/ILocationService.cs
@@ -2,4 +2,6 @@ public interface ILocationService
 {
     public Task<Location[]> GetLocations();
     public Task<Location?> GetLocationById(int id);
+    public Task<bool> LocationIsValid(Location location);
+    public Task AddLocation(Location location);
 }
