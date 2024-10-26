1e1b6fb96a02cb75f7abeb5992eb507f389ec4ef
Max Bretherton <maxob2005@gmail.com>
2024-10-26 18:53:02 +0200

    Add and move shipment validation to separate service #163

--- CargoHub/api/Services/CRUD/ShipmentService.cs
+++ CargoHub/api/Services/CRUD/ShipmentService.cs
@@ -21,26 +21,12 @@ public class ShipmentService : IShipmentService
         return shipment.Items.ToArray();
     }
 
-    public async Task<bool> AddShipment(Shipment shipment){
-        // Check if shipment is valid
-        /*
-         * all items exist
-         * order id is valid
-         * date is valid
-         * ship_to and bill_to are valid
-         etc.
-        */
-
-        // Check if shipment id is already in use
-        Shipment[] shipments = _shipmentProvider.Get();
-        if (shipments.Any(ord => ord.Id == shipment.Id)) return false;
-
+    public async Task AddShipment(Shipment shipment){
         string now = shipment.GetTimeStamp();
         shipment.CreatedAt = now;
         shipment.UpdatedAt = now;
         _shipmentProvider.Add(shipment);
         await _shipmentProvider.Save();
-        return true;
     }
 
     public async Task DeleteShipment(Shipment shipment){
@@ -48,18 +34,12 @@ public class ShipmentService : IShipmentService
         await _shipmentProvider.Save();
     }
 
-    public async Task<bool> ReplaceShipment(Shipment shipment, int shipmentId){
-        // check if shipment is valid (like in AddShipment), else return false
-        // so, should probably be a seperate method/service to check when a shipment is valid
-
+    public async Task ReplaceShipment(Shipment shipment, int shipmentId){
         string now = shipment.GetTimeStamp();
         shipment.CreatedAt = now;
         shipment.UpdatedAt = now;
 
-        // will return false if there is no shipment with the same id
-        if (!_shipmentProvider.Replace(shipment, shipmentId)) return false;
+        _shipmentProvider.Replace(shipment, shipmentId);
         await _shipmentProvider.Save();
-
-        return true;
     }
 }
