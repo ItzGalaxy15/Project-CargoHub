dbbc5b9a6e09939e7925d499bc854b42a3f65e4b
Amer Alhasoun <ameralhasoun@gmail.com>
2024-11-08 17:59:29 +0100

    "Remove warnings ItemGroupController"

--- CargoHub/api/Controllers/ItemGroupController.cs
+++ CargoHub/api/Controllers/ItemGroupController.cs
@@ -18,20 +18,22 @@ public class ItemGroupController : Controller
     [HttpGet]
     public async Task<IActionResult> GetItemGroups()
     {
-        return Ok(_itemGroupService.GetItemGroups());
+        ItemGroup[] itemGroups = await Task.Run(() => _itemGroupService.GetItemGroups());
+        return Ok(itemGroups);
     }
     
     [HttpGet("{id}")]
     public async Task<IActionResult> GetItemGroupById(int id)
     {
-        var itemGroup = _itemGroupService.GetItemGroupById(id);
+        
+        ItemGroup? itemGroup = await Task.Run(() => _itemGroupService.GetItemGroupById(id));
         return itemGroup is null ? BadRequest() : Ok(itemGroup);
     }
 
     [HttpGet("{id}/items")]
     public async Task<IActionResult> GetItemsForItemGroups(int id) // id = itemGroupId
     {
-        Item[] items = _itemService.GetItemsForItemGroups(id);
+        Item[] items = await Task.Run(() => _itemService.GetItemsForItemGroups(id));
         return Ok(items);
     }
 
