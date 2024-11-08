a2cc8185816740f1d34af45d28d5151193e83dce
ItzGalaxy15 <152269683+ItzGalaxy15@users.noreply.github.com>
2024-11-08 15:33:54 +0100

    Sorted types together

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -4,12 +4,17 @@ builder.Services.AddControllers();
 
 builder.Services.AddTransient<IClientProvider, ClientProvider>();
 builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IClientValidation, ClientValidation>();
 
 builder.Services.AddTransient<ILocationProvider, LocationProvider>();
 builder.Services.AddScoped<ILocationService, LocationService>();
+builder.Services.AddScoped<ILocationValidation, LocationValidation>();
+
 
 builder.Services.AddTransient<IItemTypeProvider, ItemTypeProvider>();
 builder.Services.AddScoped<IItemTypeService, ItemTypeService>();
+builder.Services.AddScoped<IItemTypeValidation, ItemTypeValidation>();
+
 
 // builder.Services.AddTransient<IOrderProvider, OrderProvider>();
 // builder.Services.AddScoped<IOrderService, OrderService>();
@@ -17,6 +22,8 @@ builder.Services.AddScoped<IItemTypeService, ItemTypeService>();
 
 builder.Services.AddTransient<ISupplierProvider, SupplierProvider>();
 builder.Services.AddScoped<ISupplierService, SupplierService>();
+builder.Services.AddScoped<ISupplierValidationService, SupplierValidationService>();
+
 
 // builder.Services.AddTransient<IShipmentProvider, ShipmentProvider>();
 // builder.Services.AddScoped<IShipmentService, ShipmentService>();
@@ -32,33 +39,21 @@ builder.Services.AddScoped<IItemValidationService, ItemValidationService>();
 
 builder.Services.AddTransient<IWarehouseProvider, WarehouseProvider>();
 builder.Services.AddScoped<IWarehouseService, WarehouseService>();
+builder.Services.AddScoped<IWarehouseValidationService, WarehouseValidationService>();
+
 
 builder.Services.AddTransient<IItemGroupProvider,ItemGroupProvider>();
 builder.Services.AddScoped<IItemGroupService, ItemGroupService>();
-
-//builder.Services.AddSingleton<IInventoryProvider, InventoryProvider>(); error
-//builder.Services.AddScoped<IInventoryService, InventoryService>();
-
-//builder.Services.AddSingleton<IItemLineProvider, ItemLineProvider>(); erorr
-//builder.Services.AddScoped<IItemLineService, ItemLineService>();
-
-builder.Services.AddScoped<IClientValidation, ClientValidation>();
-builder.Services.AddScoped<ILocationValidation, LocationValidation>();
-builder.Services.AddScoped<IItemTypeValidation, ItemTypeValidation>();
-// builder.Services.AddScoped<IOrderValidationService, OrderValidationService>();
-builder.Services.AddScoped<ISupplierValidationService, SupplierValidationService>();
-//builder.Services.AddScoped<IShipmentValidationService, ShipmentValidationService>();
-builder.Services.AddScoped<ITransferValidationService, TransferValidationService>();
-//builder.Services.AddScoped<IItemValidationService, ItemValidationService>();
-builder.Services.AddScoped<IWarehouseValidationService, WarehouseValidationService>();
 builder.Services.AddScoped<IItemGroupValidationService, ItemGroupValidationService>();
 
-builder.Services.AddTransient<IInventoryProvider, InventoryProvider>();
-builder.Services.AddScoped<IInventoryService, InventoryService>();
-builder.Services.AddScoped<IInventoryValidationService, InventoryValidationService>();
 
-builder.Services.AddTransient<IItemLineProvider, ItemLineProvider>();
-builder.Services.AddScoped<IItemLineService, ItemLineService>();                               
+builder.Services.AddSingleton<IInventoryProvider, InventoryProvider>();
+builder.Services.AddScoped<IInventoryService, InventoryService>();
+builder.Services.AddTransient<IInventoryProvider, InventoryProvider>();
+
+
+builder.Services.AddSingleton<IItemLineProvider, ItemLineProvider>();
+builder.Services.AddScoped<IItemLineService, ItemLineService>();                            
 builder.Services.AddScoped<IItemLineValidationService, ItemLineValidationService>();
 
 
