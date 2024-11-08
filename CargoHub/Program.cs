e06c4521c9ca85b7bdea56583e109f23b61fc302
Jordy <jordym_1999@hotmail.com>
2024-11-08 15:18:30 +0100

    comments out errors

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -4,51 +4,52 @@ builder.Services.AddControllers();
 
 builder.Services.AddSingleton<IClientProvider, ClientProvider>();
 builder.Services.AddScoped<IClientService, ClientService>();
-builder.Services.AddScoped<IClientValidation, ClientValidation>();
 
 builder.Services.AddSingleton<ILocationProvider, LocationProvider>();
 builder.Services.AddScoped<ILocationService, LocationService>();
-builder.Services.AddScoped<ILocationValidation, LocationValidation>();
 
 builder.Services.AddSingleton<IItemTypeProvider, ItemTypeProvider>();
 builder.Services.AddScoped<IItemTypeService, ItemTypeService>();
-builder.Services.AddScoped<IItemTypeValidation, ItemTypeValidation>();
 
-builder.Services.AddSingleton<IOrderProvider, OrderProvider>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IOrderValidationService, OrderValidationService>();
+// builder.Services.AddSingleton<IOrderProvider, OrderProvider>(); errors
+// builder.Services.AddScoped<IOrderService, OrderService>();
 
 builder.Services.AddSingleton<ISupplierProvider, SupplierProvider>();
 builder.Services.AddScoped<ISupplierService, SupplierService>();
-builder.Services.AddScoped<ISupplierValidationService, SupplierValidationService>();
 
-builder.Services.AddSingleton<IShipmentProvider, ShipmentProvider>();
-builder.Services.AddScoped<IShipmentService, ShipmentService>();
-builder.Services.AddScoped<IShipmentValidationService, ShipmentValidationService>();
+// builder.Services.AddSingleton<IShipmentProvider, ShipmentProvider>();  errorr
+// builder.Services.AddScoped<IShipmentService, ShipmentService>();
 
 builder.Services.AddSingleton<ITransferProvider, TransferProvider>();
 builder.Services.AddScoped<ITransferService, TransferService>();
 
-builder.Services.AddSingleton<IItemProvider, ItemProvider>();
-builder.Services.AddScoped<IItemService, ItemService>();
-builder.Services.AddScoped<IItemValidationService, ItemValidationService>();
+//builder.Services.AddSingleton<IItemProvider, ItemProvider>(); errors
+//builder.Services.AddScoped<IItemService, ItemService>();
 
 builder.Services.AddSingleton<IWarehouseProvider, WarehouseProvider>();
 builder.Services.AddScoped<IWarehouseService, WarehouseService>();
-builder.Services.AddScoped<IWarehouseValidationService, WarehouseValidationService>();
-
 
 builder.Services.AddSingleton<IItemGroupProvider,ItemGroupProvider>();
 builder.Services.AddScoped<IItemGroupService, ItemGroupService>();
+
+//builder.Services.AddSingleton<IInventoryProvider, InventoryProvider>(); error
+//builder.Services.AddScoped<IInventoryService, InventoryService>();
+
+//builder.Services.AddSingleton<IItemLineProvider, ItemLineProvider>(); erorr
+//builder.Services.AddScoped<IItemLineService, ItemLineService>();
+
+builder.Services.AddScoped<IClientValidation, ClientValidation>();
+builder.Services.AddScoped<ILocationValidation, LocationValidation>();
+builder.Services.AddScoped<IItemTypeValidation, ItemTypeValidation>();
+// builder.Services.AddScoped<IOrderValidationService, OrderValidationService>();
+builder.Services.AddScoped<ISupplierValidationService, SupplierValidationService>();
+//builder.Services.AddScoped<IShipmentValidationService, ShipmentValidationService>();
+builder.Services.AddScoped<ITransferValidationService, TransferValidationService>();
+//builder.Services.AddScoped<IItemValidationService, ItemValidationService>();
+builder.Services.AddScoped<IWarehouseValidationService, WarehouseValidationService>();
 builder.Services.AddScoped<IItemGroupValidationService, ItemGroupValidationService>();
-
-builder.Services.AddSingleton<IInventoryProvider, InventoryProvider>();
-builder.Services.AddScoped<IInventoryService, InventoryService>();
-builder.Services.AddScoped<IInventoryValidationService, InventoryValidationService>();
-
-builder.Services.AddSingleton<IItemLineProvider, ItemLineProvider>();
-builder.Services.AddScoped<IItemLineService, ItemLineService>();
-builder.Services.AddScoped<IItemLineValidationService, ItemLineValidationService>();
+//builder.Services.AddScoped<IInventoryValidationService, InventoryValidationService>();
+//builder.Services.AddScoped<IItemLineValidationService, ItemLineValidationService>();
 
 
 var app = builder.Build();
