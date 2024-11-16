165edd8f564403bdd6b27563439eb0dac77fb27b
Amer Alhasoun <ameralhasoun@gmail.com>
2024-11-16 19:09:08 +0100

    "Add ModifyItemGroup() To IItemGroupService #207"

--- CargoHub/apiV2/Services/CRUD/ItemGroup/IItemGroupService.cs
+++ CargoHub/apiV2/Services/CRUD/ItemGroup/IItemGroupService.cs
@@ -7,5 +7,6 @@ namespace apiV2.Interfaces
         public Task AddItemGroup(ItemGroup itemGroup);
         public Task DeleteItemGroup(ItemGroup itemGroup);
         public Task ReplaceItemGroup(ItemGroup itemGroup, int itemGroupId);
+        public Task ModifyItemGroup(int id, Dictionary<string, dynamic> patch, ItemGroup itemGroup);
     }
 }
