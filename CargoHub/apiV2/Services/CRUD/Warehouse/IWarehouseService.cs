92cb79360f21eb6a6fcdc5a04fcd7094226c5868
Amer Alhasoun <ameralhasoun@gmail.com>
2024-11-16 16:36:20 +0100

    "Add ModifyWarehouse() to IWarehouseService"

--- CargoHub/apiV2/Services/CRUD/Warehouse/IWarehouseService.cs
+++ CargoHub/apiV2/Services/CRUD/Warehouse/IWarehouseService.cs
@@ -7,5 +7,6 @@ namespace apiV2.Interfaces
         public Task AddWarehouse(Warehouse warehouse);
         public Task DeleteWarehouse(Warehouse warehouse);
         public Task ReplaceWarehouse(Warehouse warehouse, int warehouseId);
+        public Task ModifyWarehouse(int id, Dictionary<string, dynamic> patch, Warehouse warehouse);
     }
 }
