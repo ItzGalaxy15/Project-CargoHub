e2dd07e7b3452aae193fa5ce2c3053face32f378
Amer Alhasoun <ameralhasoun@gmail.com>
2024-11-16 18:20:06 +0100

    Add IsInventoryValidForPatch() to IInventoryValidationService

--- CargoHub/apiV2/Services/Validation/Inventory/IInventoryValidationService.cs
+++ CargoHub/apiV2/Services/Validation/Inventory/IInventoryValidationService.cs
@@ -3,5 +3,6 @@ namespace apiV2.ValidationInterfaces
     public interface IInventoryValidationService
     {
         public bool IsInventoryValid(Inventory? inventory, bool update = false);
+        public bool IsInventoryValidForPatch(Dictionary<string, dynamic> patch);
     }
 }
