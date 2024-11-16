2a5dd90d1d2b747c464088b681f6e74b7db776a8
Amer Alhasoun <ameralhasoun@gmail.com>
2024-11-16 17:07:58 +0100

    "Add ModifyWarehouse() to WarehouseService "

--- CargoHub/apiV2/Services/CRUD/Warehouse/WarehouseService.cs
+++ CargoHub/apiV2/Services/CRUD/Warehouse/WarehouseService.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using apiV2.Interfaces;
 
 namespace apiV2.Services
@@ -44,5 +45,75 @@ namespace apiV2.Services
             _warehouseProvider.Delete(warehouse);
             await _warehouseProvider.Save();
         }
+
+        public async Task ModifyWarehouse(int id, Dictionary<string, dynamic> patch, Warehouse warehouse)
+        {
+            foreach (var (key, value) in patch)
+            {
+                if (value is JsonElement jsonElement)
+                {
+                    switch (key)
+                    {
+                        case "code":
+                            warehouse.Code = jsonElement.GetString()!;
+                            break;
+
+                        case "name":
+                            warehouse.Name = jsonElement.GetString()!;
+                            break;
+
+                        case "address":
+                            warehouse.Address = jsonElement.GetString()!;
+                            break;
+
+                        case "zip":
+                            warehouse.Zip = jsonElement.GetString()!;
+                            break;
+
+                        case "city":
+                            warehouse.City = jsonElement.GetString()!;
+                            break;
+
+                        case "province":
+                            warehouse.Province = jsonElement.GetString()!;
+                            break;
+
+                        case "country":
+                            warehouse.Country = jsonElement.GetString()!;
+                            break;
+
+                        case "contact":
+                            if (jsonElement.ValueKind == JsonValueKind.Object)
+                            {
+                                var contact = warehouse.Contact;
+                                foreach (var contactProperty in jsonElement.EnumerateObject())
+                                {
+                                    switch (contactProperty.Name)
+                                    {
+                                        case "name":
+                                            contact.Name = contactProperty.Value.GetString()!;
+                                            break;
+
+                                        case "phone":
+                                            contact.Phone = contactProperty.Value.GetString()!;
+                                            break;
+
+                                        case "email":
+                                            contact.Email = contactProperty.Value.GetString()!;
+                                            break;
+                                    }
+                                }
+                                warehouse.Contact = contact;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            warehouse.UpdatedAt = warehouse.GetTimeStamp();
+            _warehouseProvider.Replace(warehouse, id);
+            await _warehouseProvider.Save();
+        }
+
     }
 }
