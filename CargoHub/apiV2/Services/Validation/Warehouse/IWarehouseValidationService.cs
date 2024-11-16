1cce4eaf8c273bc9373770840a542c5508f067f5
Amer Alhasoun <ameralhasoun@gmail.com>
2024-11-16 16:37:08 +0100

    "Add IsWarehouseValidForPatch() to IWarehouseValidationService"

--- CargoHub/apiV2/Services/Validation/Warehouse/IWarehouseValidationService.cs
+++ CargoHub/apiV2/Services/Validation/Warehouse/IWarehouseValidationService.cs
@@ -3,5 +3,6 @@ namespace apiV2.ValidationInterfaces
     public interface IWarehouseValidationService
     {
         public bool IsWarehouseValid(Warehouse? warehouse, bool update = false);
+        public bool IsWarehouseValidForPatch(Dictionary<string, dynamic> patch);
     }
 }
