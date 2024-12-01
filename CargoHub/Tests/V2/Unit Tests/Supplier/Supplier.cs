e03e9b46fc29de1b86579c397f44fba466501292
Amer Alhasoun <ameralhasoun@gmail.com>
2024-12-01 21:31:43 +0100

    "Clean the code of  Supplier Model UnitTest #229"

--- CargoHub/Tests/V2/Unit Tests/Supplier/Supplier.cs
+++ CargoHub/Tests/V2/Unit Tests/Supplier/Supplier.cs
@@ -6,62 +6,6 @@ using System.Text.Json;
 
 namespace SupplierUnitTest;
 
-public abstract class Base
-{
-    public string GetTimeStamp(){
-        var cetTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time"));
-        string cetTimeString = cetTime.ToString("s", CultureInfo.InvariantCulture).Replace('T', ' ');
-        return cetTimeString;
-    }
-
-    [JsonPropertyName("created_at")]
-    public required string CreatedAt { get; set; }
-
-    [JsonPropertyName("updated_at")]
-    public required string UpdatedAt { get; set; }
-    
-}
-public class Supplier : Base
-{
-
-    [JsonPropertyName("id")]
-    public required int Id { get; set; }
-
-    [JsonPropertyName("code")]
-    public required string Code { get; set; }
-
-    [JsonPropertyName("name")]
-    public required string Name { get; set; }
-
-    [JsonPropertyName("address")]
-    public required string Address { get; set; }
-
-    [JsonPropertyName("address_extra")]
-    public required string AddressExtra { get; set; }
-
-    [JsonPropertyName("city")]
-    public required string City { get; set; }
-
-    [JsonPropertyName("zip_code")]
-    public required string ZipCode { get; set; }
-
-    [JsonPropertyName("province")]
-    public required string Province { get; set; }
-
-    [JsonPropertyName("country")]
-    public required string Country { get; set; }
-
-    [JsonPropertyName("contact_name")]
-    public required string ContactName { get; set; }
-
-    [JsonPropertyName("phonenumber")]
-    public required string Phonenumber { get; set; }
-
-    [JsonPropertyName("reference")]
-    public required string Reference { get; set; }
-    
-}
-
 
 [TestClass]
 public class SupplierUnitTest
