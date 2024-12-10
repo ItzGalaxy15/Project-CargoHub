b45f9b8ef59cfd5ab1026438e522b26cd898442e
ItzGalaxy15 <152269683+ItzGalaxy15@users.noreply.github.com>
2024-12-10 10:05:46 +0100

    Added a main class in program

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -1,16 +1,29 @@
 using V1;
 using V2;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace CargoHub
+{
+    /// <summary>
+    /// The main entry point for the CargoHub application.
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the CargoHub application.
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-ServiceConfigurationV1.ConfigureServices(builder.Services);
-ServiceConfigurationV2.ConfigureServices(builder.Services);
+            ServiceConfigurationV1.ConfigureServices(builder.Services);
+            ServiceConfigurationV2.ConfigureServices(builder.Services);
 
-var app = builder.Build();
-app.Urls.Add("http://localhost:3000");
+            var app = builder.Build();
+            app.Urls.Add("http://localhost:3000");
 
-app.MapControllers();
+            app.MapControllers();
 
-app.UseApiKeyAuthorization();
+            app.UseApiKeyAuthorization();
 
-app.Run();
+            app.Run();
+        }
+    }
+}
