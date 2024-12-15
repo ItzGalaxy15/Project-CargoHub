e4f9f95d977fe3ebaf8d0a44ec6fbc1609e0ed49
Amer Alhasoun <ameralhasoun@gmail.com>
2024-12-15 12:12:58 +0100

    "Add swagger.json && swagger. yaml#294"

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -3,11 +3,19 @@ using V2;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Swagger generator
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 ServiceConfigurationV1.ConfigureServices(builder.Services);
 ServiceConfigurationV2.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 app.Urls.Add("http://localhost:3000");
+
+// Enable Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
