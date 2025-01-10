c59938be1b64a93e1104c11a750400e6b2cd8a01
Amer Alhasoun <ameralhasoun@gmail.com>
2025-01-10 14:11:28 +0100

    "replace the logging middleware from the program to  Middleware foler #304"

--- CargoHub/Program.cs
+++ CargoHub/Program.cs
@@ -24,33 +24,7 @@ app.UseSwaggerUI();
 app.UseApiKeyAuthorization();
 
 // Logging middleware
-app.Use(async (context, next) =>
-{
-    var logFileOptions = context.RequestServices.GetService<IOptions<LogFileOptions>>()?.Value ??
-        new LogFileOptions { LogPath = "Logs/RequestLogs.txt" };
-
-    var logDirectory = Path.GetDirectoryName(logFileOptions.LogPath);
-    if (!string.IsNullOrEmpty(logDirectory) && !Directory.Exists(logDirectory))
-    {
-        Directory.CreateDirectory(logDirectory);
-    }
-
-    if (!File.Exists(logFileOptions.LogPath))
-    {
-        await File.WriteAllTextAsync(logFileOptions.LogPath, string.Empty);
-    }
-
-    await File.AppendAllTextAsync(
-        logFileOptions.LogPath,
-        $"\n{DateTime.Now} - {context.Connection.RemoteIpAddress} requested {context.Request.Method} {context.Request.GetDisplayUrl()}");
-
-    await next.Invoke();
-
-    await File.AppendAllTextAsync(
-        logFileOptions.LogPath,
-        $"\t | \tResponded with status code: {context.Response.StatusCode}");
-});
-
+app.UseLoggingMiddleware();
 app.MapControllers();
 
 app.Run();
